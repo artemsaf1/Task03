e2aec75cb2c33d9b1983433992a6ed48b644f24c
artemsaf1 <Art.Bobk@yandex.ru>
2025-02-15 20:05:28 +0300

    g

---
+++ Task03_1/src/max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main () {
+    int a,b;
+    char newline;
+    if (scanf("%d %d%c", &a, &b, &newline) != 3 || newline != '\n') {
+        printf("n/a");
+        return 1;
+    }
+
+    printf("%d", max(a,b));
+
+    return 0;
+}
