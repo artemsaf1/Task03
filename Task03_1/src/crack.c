7adb775fae31fb2b633339beaa395d946f19645a
artemsaf1 <Art.Bobk@yandex.ru>
2025-02-15 22:13:14 +0300

    g

---
+++ Task03_1/src/crack.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int incide(double x, double y) {
+    return (x * x + y * y) < 1.0;
+}
+
+int main() {
+    double x, y;
+    char newline;
+    
+    if(scanf("%lf %lf%c",&x, &y, &newline) != 3 || newline != '\n') {
+        printf("n/a");
+        return 1;
+    }
+
+    if (incide(x, y)) {
+        printf("GOTCHA");
+    } else {
+        printf("MISS");
+    }
+    return 0;
+}
