5e688a8ff731c438b413ecb521d3b65c0de9bd7c
artemsaf1 <Art.Bobk@yandex.ru>
2025-02-15 19:48:34 +0300

    g

--- Task03_1/src/arithmetic.c
+++ Task03_1/src/arithmetic.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int sum(int a, int b);
-
-int main()
-{
+int main() {
+    int a, b;
+    char newline;
+    if (scanf("%d %d%c",&a, &b, &newline) != 3 || newline != '\n') {
+        printf("n/a");
+        return 1;
+    }
+    if (b != 0) {
+        printf("%d %d %d %d", a + b, a - b, a * b, a / b);
+    } else {
+        printf("%d %d %d n/a", a + b, a - b, a * b);
+    }
+    
     return 0;
-}
-
-int sum(int a, int b){
-    return (2 * a * b) / (a - b);
 }
